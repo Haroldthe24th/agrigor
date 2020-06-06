0f6b3ec5e6d752d70c90e3da3a1293ee1db97219
bnaness <scriptshock.dev@gmail.com>
2020-06-06 19:34:19 +0100

    heroku attempt #4

--- react/src/App.js
+++ react/src/App.js
@@ -130,7 +130,7 @@ class App extends React.Component {
       }
     });
 
-    await fetch("http://localhost:3000/resources/getResources/mixed", {
+    await fetch("http://localhost:5000/resources/getResources/mixed", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -157,7 +157,7 @@ class App extends React.Component {
       return;
     }
     //
-    const url = "http://localhost:3000/resources/getResources/" + section;
+    const url = "http://localhost:5000/resources/getResources/" + section;
 
     let req = new Request(url);
     await fetch(req).then((response) => {
